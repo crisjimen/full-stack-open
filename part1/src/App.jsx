77cdf5289e6182ea0ff902a9d8fcc8a141bd1155
crisjimen <182010261+crisjimen@users.noreply.github.com>
2026-09-06 13:43:41 +0200

    Exercise 1.1 uploaded

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,24 +1,35 @@
-//Definición de un componente
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
+const Header = (props) => {
+  return <h1>{props.course}</h1>
 }
 
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )}
+
+  const Total = (props) => {
+    return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  }
+
+
 const App = () => {
-  //Se pueden usar también valores definidos en constantes
-  const name="Cris";
-  const age = 21;
+  const course = 'half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <p>Hello world</p>
-      //Utilización del componente definido anteriormente
-      <Hello name="Marta" age="25" />
-      <Hello name="Javier" age="40"/>
-      <Hello name={name} age={age}/>
+      <Header course = {course} />
+      <Content part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3} />
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
     </div>
   )
 }
